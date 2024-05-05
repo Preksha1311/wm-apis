288a6c816d4a83ff89a9fc0a89098a4d34f60811
Preksha <prekshaparas1311@gmail.com>
2024-05-05 19:24:18 +0530

    NODE_ENV

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express from "express"
+import express,{ Request, Response, NextFunction} from "express"
+import { HttpError } from "http-errors";
+import { config } from './config/config';
 const app = express()
 // const port = 3000
 
-app.get("/", (req, res,next) => {
+app.get("/", (req ,res ,next) => {
   res.json({message : "welcome"});
+})
+
+app.use((err: HttpError ,req:Request,res:Response ,next:NextFunction) =>{
+  const statusCode = err.statusCode || 500;
+
+  return res.status(statusCode).json({
+    message : err.message,
+    errorStack : config.env === 'development' ? err.stack : "",
+  })
 })
 
 export default app;
--- src/config/db.ts
+++ src/config/db.ts
@@ -7,15 +7,13 @@ const dbConnect = async () => {
     try {
         mongoose.connect(config.databaseUrl as string)
         mongoose.connection.on("connected",()=>{
-            console.log("connected to the database successfullly");
+            console.log("connected to the database successfully");
             
         }),
         //for future db error
         mongoose.connection.on("error",(err)=>{
             console.log("Error in connection to DB",err);
-            
         })
-
     } catch (err) {
         console.log("Failed to connect to databse ",err);
         process.exit(1);
